6f480b21a244845a6ed092c0a3dfa95bd15f39b8
Tiago Gomes <tiagogomes772@gmail.com>
2016-11-17 15:27:30 +0000

    Inserted Algorithm with errors
    There are some things to check before running the algorithm that i did
    know

--- IAJ Lab 6/Assets/Scripts/IAJ.Unity/DecisionMaking/MCTS/MCTS.cs
+++ IAJ Lab 6/Assets/Scripts/IAJ.Unity/DecisionMaking/MCTS/MCTS.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Scripts.IAJ.Unity.DecisionMaking.GOB;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.IAJ.Unity.DecisionMaking.MCTS
@@ -64,9 +65,17 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.MCTS
 
             var startTime = Time.realtimeSinceStartup;
             this.CurrentIterationsInFrame = 0;
+            int Ntimes = 1000; //TODO Dont know where to get this number
+            int counter = 0;
+            while(counter < Ntimes)
+            {
+                selectedNode = Selection(this.InitialNode);
+                reward = Playout(selectedNode.State);
+                Backpropagate(selectedNode, reward);
+                counter++;
+            }
 
-            //TODO: implement
-            throw new NotImplementedException();
+            return BestChild(this.InitialNode).Action;
         }
 
         private MCTSNode Selection(MCTSNode initialNode)
@@ -76,41 +85,92 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.MCTS
             MCTSNode bestChild;
 
 
-            //TODO: implement
-            throw new NotImplementedException();
+            while(currentNode.State.GetExecutableActions().Length > 0)
+            { 
+                GOB.Action[] allActionAvailable = currentNode.State.GetExecutableActions();
+
+                int childNodesNumber = currentNode.ChildNodes.Count;
+                if (allActionAvailable.Length > childNodesNumber)
+                {
+                    currentNode.ChildNodes.Add(Expand(currentNode, allActionAvailable[childNodesNumber]));
+                }
+                else
+                {
+                    currentNode = BestChild(currentNode);
+                }
+            }
+            return currentNode;
         }
 
         private Reward Playout(WorldModel initialPlayoutState)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            GOB.Action action;
+            while (initialPlayoutState.IsTerminal())
+            {
+                int index = RandomGenerator.Next(initialPlayoutState.GetExecutableActions().Length);
+                action = initialPlayoutState.GetExecutableActions()[index];
+                //TODO Dont know how to get a new State from a given state and an action
+                initialPlayoutState = new WorldModel(initialPlayoutState);
+            }
+            Reward r = new Reward();
+            //TODO Verify if reward is this score
+            r.Value = initialPlayoutState.GetScore();
+            return r;
         }
 
         private void Backpropagate(MCTSNode node, Reward reward)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            while(!node.Equals(null))
+            {
+                node.N++;
+                //TODO VERIFY this
+                node.Q = node.Q + reward.Value;
+                node = node.Parent;
+            }
         }
 
         private MCTSNode Expand(MCTSNode parent, GOB.Action action)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            //TODO Compare with professor implementation
+            MCTSNode new_child = new MCTSNode(parent.State);
+            new_child.Action = action;
+            return new_child;
         }
 
         //gets the best child of a node, using the UCT formula
         private MCTSNode BestUCTChild(MCTSNode node)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+           
+            MCTSNode bestChild = node.ChildNodes[0];
+            int bestNodeValue = (int)bestChild.Q + ((int)Math.Sqrt(2) * (int)Math.Sqrt(Math.Log(node.N) / bestChild.N));
+            MCTSNode auxChild;
+            int auxNodeValue;
+            for (int i=1; i < node.ChildNodes.Count; i++)
+            {
+                auxChild = node.ChildNodes[i];
+                auxNodeValue = (int)auxChild.Q + ((int)Math.Sqrt(2) * (int)Math.Sqrt(Math.Log(node.N) / auxChild.N));
+                if (auxNodeValue > bestNodeValue)
+                    bestChild = auxChild;  
+            }
+            return bestChild;
         }
 
         //this method is very similar to the bestUCTChild, but it is used to return the final action of the MCTS search, and so we do not care about
         //the exploration factor
         private MCTSNode BestChild(MCTSNode node)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            MCTSNode bestChild = node.ChildNodes[0];
+            int bestNodeValue = (int)bestChild.Q + ((int)Math.Sqrt(2) * (int)Math.Sqrt(Math.Log(node.N) / bestChild.N));
+            MCTSNode auxChild;
+            int auxNodeValue;
+            for (int i = 1; i < node.ChildNodes.Count; i++)
+            {
+                auxChild = node.ChildNodes[i];
+                auxNodeValue = (int)auxChild.Q + (int)Math.Sqrt(Math.Log(node.N) / auxChild.N);
+                if (auxNodeValue > bestNodeValue)
+                    bestChild = auxChild;
+            }
+            return bestChild;
         }
     }
 }
